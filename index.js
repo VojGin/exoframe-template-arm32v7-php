df46bc18416fa2c6a4d5b533a61735e34c60fa68
Vojtěch Gintner <gintnervojtech@gmail.com>
2018-10-10 14:58:33 +0200

    init

---
+++ index.js
@@ -0,0 +1,57 @@
+// npm packages
+const fs = require('fs');
+const path = require('path');
+
+const phpDockerfile = `FROM arm32v7/php:7.2-apache
+COPY . /var/www/html/
+RUN chmod -R 755 /var/www/html/
+EXPOSE 80
+EXPOSE 443
+`;
+
+// template name
+exports.name = 'static';
+
+// function to check if the template fits this recipe
+exports.checkTemplate = async ({tempDockerDir}) => {
+  // if project already has dockerfile - just exit
+  try {
+    const filesList = fs.readdirSync(tempDockerDir);
+    if (filesList.includes('index.php')) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return false;
+  }
+};
+
+// function to execute current template
+exports.executeTemplate = async ({username, tempDockerDir, resultStream, util, docker, existing}) => {
+  try {
+    // generate dockerfile
+    const dockerfile = phpDockerfile;
+    const dfPath = path.join(tempDockerDir, 'Dockerfile');
+    fs.writeFileSync(dfPath, dockerfile, 'utf-8');
+    util.writeStatus(resultStream, {message: 'Deploying PHP Apache project..', level: 'info'});
+
+    // build docker image
+    const buildRes = await docker.build({username, resultStream});
+    util.logger.debug('Build result:', buildRes);
+
+    // start image
+    const container = await docker.start(Object.assign({}, buildRes, {username, existing, resultStream}));
+    util.logger.debug(container);
+
+    // clean temp folder
+    await util.cleanTemp();
+
+    // return new deployments
+    util.writeStatus(resultStream, {message: 'Deployment success!', deployments: [container], level: 'info'});
+    resultStream.end('');
+  } catch (e) {
+    util.logger.debug('build failed!', e);
+    util.writeStatus(resultStream, {message: e.error, error: e.error, log: e.log, level: 'error'});
+    resultStream.end('');
+  }
+};
